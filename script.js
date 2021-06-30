9dd7bcb4b600a51f821663c25c0018df21965a9b
Muhammed Ashir <ashir@webandcrafts.in>
2021-06-30 08:50:14 +0530

    local storage added, delete functionality added, sidemenu toggle added

--- script.js
+++ script.js
@@ -1,87 +1,94 @@
-let notes = document.getElementById('notes');
+showNotes();
 let title = document.getElementById("title");
 let content = document.getElementById("content");
 let addNote = document.getElementById('addNote');
-let count = 0;
 
 addNote.addEventListener('submit',function(event){
     event.preventDefault();
-    count++;
 
-    let note = document.createElement('div');
-    note.className = 'note';
-    note.id = 'note' + count;
+    let n_Obj;
+    let n_item = localStorage.getItem("notes");
+    if(n_item == null){
+        n_Obj = [];
+    }
+    else{
+        n_Obj = JSON.parse(n_item);
+    }
+    n_Obj.push({title:title.value, content:content.value,color:'#C2F9BB'});
+    localStorage.setItem("notes", JSON.stringify(n_Obj));
 
-    let noteTitle = document.createElement('h2');
-    noteTitle.className = 'note-title';
-    noteTitle.innerHTML=title.value;
-    note.appendChild(noteTitle);
-
-    let noteContent = document.createElement('p');
-    noteContent.className = 'note-content';
-    noteContent.innerHTML=content.value;
-    note.appendChild(noteContent);
-
-    let options = document.createElement('button');
-    options.className = 'option-button';
-    options.innerHTML = '<i class="fas fa-align-left"></i>';
-    options.onclick = noteOption;
-    note.appendChild(options);
-
-    notes.appendChild(note);
-
-    // location.reload();
+    location.reload();
     addNote.reset();
 
 })
 
+function showNotes(){
 
-function noteOption() {
-
-    // let x = document.getElementById('note-m');
-    // let x1 = document.getElementById('b1');
-    // if (x.style.display === "none") {
-    //     x.style.display = "inline-block";
-    //     b1.innerHTML='<i class="fas fa-times"></i>';
-    //   } else {
-    //     x.style.display = "none";
-    //     b1.innerHTML='<i class="fas fa-align-left"></i>';
-    //   }
-
-    let menus = document.getElementsByClassName('note-menu');
-    for (let i = 0; i < menus.length; i++) {
-        menus[i].remove();
+    let n_Obj;
+    let n_item = localStorage.getItem("notes");
+    if(n_item == null){
+        n_Obj = [];
+    }
+    else{
+        n_Obj = JSON.parse(n_item);
     }
 
-    let noteMenu = document.createElement('div'); 
-    noteMenu.className = "note-menu";
-    
-    let colors = [
-        '#C2F9BB',
-        '#87F5FB',
-        '#FAF0CA'
-    ];
+    let html = '';
+    let notes = document.getElementById('notes');
 
-    colors.forEach(color => {
-        let colorOption = document.createElement('button');
-        colorOption.className = "color-option";
-        colorOption.style.backgroundColor = color;
-        colorOption.onclick = setColor;
-        noteMenu.appendChild(colorOption);
+    n_Obj.forEach((item, index) => {
+
+        html += `<div class="note">
+                    <h2 class="note-title">${item.title}</h2>
+                    <p class="note-content">
+                        ${item.content}
+                    </p>
+                    <button class="option-button" id="menuButton" onclick="menuAction(${index})"><i class="fas fa-align-left"></i></button>
+                    <div class="note-menu" id="${index}">
+                        <button class="color-option"></button>
+                        <button class="color-option"></button>
+                        <button class="color-option"></button>
+                        <div class="delete-note">
+                            <a href="" class="btn" onclick="deleteitem(${index})"><i class="fa fa-trash" style="color:#d11a2a;"></i></a>
+                        </div>
+                    </div>
+                </div>`;
     });
 
-    let deleteButton = document.createElement('div');
-    deleteButton.className = 'delete-note';
-    deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
-    noteMenu.appendChild(deleteButton);
-
-    this.parentNode.appendChild(noteMenu); 
+    notes.innerHTML = html;
 
 }
 
-function setColor() {
-
-    let note = this.parentNode.parentNode;
-    let newColor = this.style.backgroundColor;
-    note.style.backgroundColor = newColor;
+function deleteitem(index){
+    if(confirm('Do you want to Delete')){
+        let n_item = localStorage.getItem("notes");
+        let n_Obj = JSON.parse(n_item);
+        n_Obj.splice(index, 1);
+        localStorage.setItem("notes", JSON.stringify(n_Obj));
+        showNotes();
+    }
 }
+
+function menuAction(index){
+    
+    let i = String(index);
+    let x = document.getElementById(i);
+    // let menuButton = document.getElementById("menuButton");
+    if (x.style.display === "none") {
+        x.style.display = "inline-block";
+        // menuButton.innerHTML='<i class="fas fa-times"></i>';
+      } else {
+        x.style.display = "none";
+        // menuButton.innerHTML='<i class="fas fa-align-left"></i>';
+      }
+      let menus = document.getElementsByClassName('note-menu');
+      for (let i = 0; i < menus.length; i++) {
+          if(menus[i].id != index){
+            menus[i].style.display="none";
+          }
+        
+    }
+
+}
+
+
